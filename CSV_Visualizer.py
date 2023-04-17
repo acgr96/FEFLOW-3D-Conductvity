121a8af926bf343ab921091eca3d3bd3f88793dd
Adam Calvert <72354690+acgr96@users.noreply.github.com>
2023-04-17 16:06:37 -0400

    Add files via upload

---
+++ CSV_Visualizer.py
@@ -0,0 +1,16 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas
+
+points = pandas.read_csv('MFQ_RQD.csv')
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = points['X'].values
+y = points['Y'].values
+z = points['Z'].values
+
+ax.scatter(x, y, z, c='r', marker='o')
+
+plt.show()
